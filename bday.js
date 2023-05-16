704fdec1a664f1594e8352448511d4faee9f7b64
nainansh <110327724+nainansh@users.noreply.github.com>
2023-05-16 10:23:33 +0530

    Update bday.js

--- bday.js
+++ bday.js
@@ -1,8 +1,10 @@
 const popbtn = document.querySelector('.click');
 const pop = document.querySelector('.pop');
+const emj = document.querySelector('.emoji-container');
 
 popbtn.addEventListener('click' , () => {
     pop.classList.remove('hidden');
     pop.classList.add('pop-animation');
     popbtn.classList.add('hidden')
+    emj.classList.remove('hidden')
 });
