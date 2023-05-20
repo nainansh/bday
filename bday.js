c6807f6a01d7b816abba95576c89fac4afa328c3
nainansh <110327724+nainansh@users.noreply.github.com>
2023-05-20 15:53:26 +0530

    Update bday.js

--- bday.js
+++ bday.js
@@ -1,10 +1,22 @@
 const popbtn = document.querySelector('.click');
 const pop = document.querySelector('.pop');
 const emj = document.querySelector('.emoji-container');
+const nxt = document.querySelector('.special');
+const lst = document.querySelector('.nxt')
 
 popbtn.addEventListener('click' , () => {
-    pop.classList.remove('hidden');
-    pop.classList.add('pop-animation');
-    popbtn.classList.add('hidden')
-    emj.classList.remove('hidden')
+    pop.classList.toggle('hidden');
+    pop.classList.toggle('pop-animation');
+    popbtn.classList.toggle('hidden');
+    emj.classList.toggle('hidden');
 });
+
+nxt.addEventListener('click' , () => {
+    pop.classList.toggle('hidden');
+    // popbtn.classList.toggle('hidden');
+    lst.classList.remove('hidden');
+    nxt.classList.toggle('hidden');
+});
+
+
+
